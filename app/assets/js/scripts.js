6ac6b8b3c9d470cc556362ef3feb25d53e948844
larry <larry.matte@arcbees.com>
2017-08-02 11:29:20 -0400

    start project

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -8,10 +8,23 @@
  */
 (function ($, window, document, undefined) {
 
-  'use strict';
+    'use strict';
 
-  $(function () {
-    // FastShell
-  });
+    $(function () {
+        // document.getElementById('js-responsive-main-nav').onclick = function () {
+        //     this.classList.toggle('active');
+        // }
+    });
+
+    $(".responsive-menu__icon").on("click", function (e){
+        e.preventDefault();
+        $(this).toggleClass('is-open');
+    });
+
+    $(".responsive-main-nav__parent").on("click", function (e){
+        e.preventDefault();
+        $(this).siblings(".sub-menu").slideToggle('is-active');
+        $(this).toggleClass('is-open');
+    });
 
 })(jQuery, window, document);
