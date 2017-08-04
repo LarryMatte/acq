0e5d8ccc271fbdd5b227244e4bf592b1a40ea5d9
larry <larry.matte@arcbees.com>
2017-08-04 15:13:59 -0400

    responsive menu - page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,12 +8,14 @@
         // }
     });
 
+    // responsive menu
+
     $(".responsive-menu__icon").on("click", function (e){
         e.preventDefault();
         $(this).toggleClass('is-open');
     });
 
-    $(".responsive-main-nav__parent").on("click", function (e){
+    $(".js-responsive-main-nav__parent").on("click", function (e){
         e.preventDefault();
         $(this).siblings(".sub-menu").slideToggle('is-active');
         $(this).toggleClass('is-open');
